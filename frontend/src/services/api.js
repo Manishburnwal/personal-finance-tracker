0cad56d1ffa4460a6e3d4af6b2c52b5e0845cf27
Manishburnwal <manishkumar1abc2@gmail.com>
2025-06-14 08:30:04 -0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api', // adjust if you're using a different port
+  baseURL: 'https://personal-finance-tracker-backend-p03o.onrender.com/api', // adjust if you're using a different port
 });
 
 // Attach token from localStorage to every request
